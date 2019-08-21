69e992f8eeae4ecf3ad39e49b0d3ac263cc8b7c8
Radek Holý <radek.holy@applifting.cz>
2019-08-21 14:06:50 +0200

    Reduce IProjectFilesBrowserService to readonly methods

--- src/services/model.ts
+++ src/services/model.ts
@@ -6,11 +6,6 @@ export interface IProjectFilesBrowserService {
   exists(path: string): Promise<boolean>;
   readDirectory(path: string): Promise<object>;
   readFile(path: string): Promise<string>;
-  writeFile(path: string, content: string): Promise<void>;
-  createDirectory(path: string): Promise<void>;
-  deleteDirectory(path: string): Promise<void>;
-  createFile(path: string, data: string): Promise<void>;
-  deleteFile(path: string): Promise<void>;
   isFile(path: string): Promise<boolean>;
   isDirectory(path: string): Promise<boolean>;
   getMetadata(path: string): Promise<Metadata>;
