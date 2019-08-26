387f99659acedaa906e424743a2effc7da163ff1
Adela Homolova <adela.homolova@applifting.cz>
2019-08-26 17:23:42 +0200

    WIP(reporter): Add draft of JSON reporter interface.

--- src/reporters/IReporter.ts
+++ src/reporters/IReporter.ts
@@ -1,5 +1,26 @@
-import { PracticeAndComponent } from '../model';
+import { PracticeAndComponent, PracticeImpact, ProjectComponentPlatform } from '../model';
+import { ServiceType } from '../detectors/ScanningStrategyDetector';
 
 export interface IReporter {
   report(practicesAndComponents: PracticeAndComponent[]): string;
+}
+
+export interface JSONReport {
+  repository: RepoInfo[];
+  practice: PracticeInfo[];
+}
+
+interface PracticeInfo {
+  name: string;
+  suggestion: string;
+  impact: PracticeImpact;
+  url: string;
+}
+
+interface RepoInfo {
+  owner: string;
+  repoName: string;
+  platform: ProjectComponentPlatform;
+  serviceType: ServiceType;
+  uri: string;
 }
