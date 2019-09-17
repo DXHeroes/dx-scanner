f71f79c3fa3a88422bc9287d5ec2535a842dd7da
ryzzaki <cuong.nugi@gmail.com>
2019-09-17 13:47:05 +0200

    code cleanup

--- src/detectors/Java/JavaComponentDetector.ts
+++ src/detectors/Java/JavaComponentDetector.ts
@@ -2,7 +2,7 @@ import { IProjectComponentDetector } from '../IProjectComponentDetector';
 import { injectable, inject } from 'inversify';
 import { Types } from '../../types';
 import { IPackageInspector } from '../../inspectors/IPackageInspector';
-import { LanguageAtPath, ProjectComponent, ProjectComponentFramework, ProjectComponentPlatform, ProjectComponentType } from '../../model';
+import { LanguageAtPath, ProjectComponent } from '../../model';
 
 @injectable()
 export class JavaComponentDetector implements IProjectComponentDetector {
@@ -15,16 +15,6 @@ export class JavaComponentDetector implements IProjectComponentDetector {
   }
 
   async detectComponent(langAtPath: LanguageAtPath): Promise<ProjectComponent[]> {
-    // return [
-    //   {
-    //     framework: ProjectComponentFramework.UNKNOWN,
-    //     language: langAtPath.language,
-    //     path: langAtPath.path,
-    //     platform: ProjectComponentPlatform.BackEnd,
-    //     repositoryPath: undefined,
-    //     type: ProjectComponentType.Application,
-    //   },
-    // ];
     return [];
   }
 }
--- src/inspectors/package/JavaPackageInspector.ts
+++ src/inspectors/package/JavaPackageInspector.ts
@@ -2,7 +2,6 @@ import { PackageInspectorBase } from './PackageInspectorBase';
 
 export class JavaPackageInspector extends PackageInspectorBase {
   async init(): Promise<void> {
-    //throw new Error('Method not implemented.');
     return;
   }
 
