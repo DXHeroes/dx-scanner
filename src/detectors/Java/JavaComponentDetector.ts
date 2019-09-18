5aa050462da323a96a506e76c39e3c8f3ee8679a
ryzzaki <cuong.nugi@gmail.com>
2019-09-18 12:37:13 +0200

    added an XML parser, returning component detector

--- src/detectors/Java/JavaComponentDetector.ts
+++ src/detectors/Java/JavaComponentDetector.ts
@@ -2,7 +2,7 @@ import { IProjectComponentDetector } from '../IProjectComponentDetector';
 import { injectable, inject } from 'inversify';
 import { Types } from '../../types';
 import { IPackageInspector } from '../../inspectors/IPackageInspector';
-import { LanguageAtPath, ProjectComponent } from '../../model';
+import { LanguageAtPath, ProjectComponent, ProjectComponentFramework, ProjectComponentPlatform, ProjectComponentType } from '../../model';
 
 @injectable()
 export class JavaComponentDetector implements IProjectComponentDetector {
@@ -15,6 +15,16 @@ export class JavaComponentDetector implements IProjectComponentDetector {
   }
 
   async detectComponent(langAtPath: LanguageAtPath): Promise<ProjectComponent[]> {
-    return [];
+    // @todo: add framework recognition => need an XML or GRADLE reader to determine the framework
+    return [
+      {
+        framework: ProjectComponentFramework.UNKNOWN,
+        language: langAtPath.language,
+        path: langAtPath.path,
+        platform: ProjectComponentPlatform.BackEnd,
+        repositoryPath: undefined,
+        type: ProjectComponentType.Application,
+      },
+    ];
   }
 }
--- src/inspectors/package/JavaPackageInspector.ts
+++ src/inspectors/package/JavaPackageInspector.ts
@@ -1,11 +1,44 @@
 import { PackageInspectorBase } from './PackageInspectorBase';
+import { IFileInspector } from '../IFileInspector';
+import { inject } from 'inversify';
+import { Types } from '../../types';
+import { DependencyType } from '../IPackageInspector';
+import * as xml2js from 'xml2js';
 
 export class JavaPackageInspector extends PackageInspectorBase {
+  private fileInspector: IFileInspector;
+  private hasLockfileFile!: boolean;
+
+  constructor(@inject(Types.IFileInspector) fileInspector: IFileInspector) {
+    super();
+    this.fileInspector = fileInspector;
+  }
+
   async init(): Promise<void> {
-    return;
+    try {
+      this.debug('JavaPackageInspector init started');
+      const mavenFileString = await this.fileInspector.readFile('pom.xml');
+      if (!mavenFileString) {
+        const gradleFileString = await this.fileInspector.readFile('build.gradle');
+      }
+      const mavenJSON = xml2js.parseString(mavenFileString, (err, result) => {
+        // testing the XML parser
+        console.log(result);
+        console.log(result.project.dependencies);
+        console.log(result.project.dependencies.dependency);
+        console.log(result.project.dependencies[0]);
+        console.log(result.project.dependencies[1]);
+      });
+      this.packages = [];
+      return;
+    } catch {}
   }
 
   hasLockfile(): boolean {
     return false;
+  }
+
+  private addPackages(dependencies: { [name: string]: string } | undefined, depType: DependencyType) {
+    return;
   }
 }
