f7f7d69c358e2b395be92010ee7eaf7aaaf005dc
ryzzaki <cuong.nugi@gmail.com>
2019-09-20 22:15:23 +0200

    interface for pom.xml parser

--- src/inspectors/package/JavaPackageInspector.ts
+++ src/inspectors/package/JavaPackageInspector.ts
@@ -17,28 +17,88 @@ export class JavaPackageInspector extends PackageInspectorBase {
   async init(): Promise<void> {
     try {
       this.debug('JavaPackageInspector init started');
+      this.packages = [];
+      const parsedDependencies: ParsedDependency[] = [];
       const mavenFileString = await this.fileInspector.readFile('pom.xml');
       if (!mavenFileString) {
         const gradleFileString = await this.fileInspector.readFile('build.gradle');
       }
-      const mavenJSON = xml2js.parseString(mavenFileString, (err, result) => {
-        // testing the XML parser
-        console.log(result);
-        console.log(result.project.dependencies);
-        console.log(result.project.dependencies.dependency);
-        console.log(result.project.dependencies[0]);
-        console.log(result.project.dependencies[1]);
+      xml2js.parseString(mavenFileString, (err, result: PomXML) => {
+        const xmlDependencies = result.project.dependencies.values();
+        for (const xmlDependency of xmlDependencies) {
+          const dependencyAttributes = xmlDependency.dependency.values();
+          for (const attribute of dependencyAttributes) {
+            parsedDependencies.push({ packageName: attribute.artifactId[0], version: attribute.version[0] });
+          }
+        }
+        this.addPackages(parsedDependencies, DependencyType.Runtime);
+        this.debug('JSPackageInspector init ended');
       });
-      this.packages = [];
-      return;
-    } catch {}
+    } catch (e) {
+      this.packages = undefined;
+      this.debug(e);
+    }
   }
 
   hasLockfile(): boolean {
     return false;
   }
 
-  private addPackages(dependencies: { [name: string]: string } | undefined, depType: DependencyType) {
-    return;
+  private addPackages(dependencies: ParsedDependency[] | undefined, depType: DependencyType) {
+    if (!dependencies) {
+      return;
+    }
+    if (!this.packages) {
+      this.packages = [];
+    }
+    for (const dependency of dependencies) {
+      const parsedVersion = PackageInspectorBase.semverToPackageVersion(dependency.version);
+      if (parsedVersion) {
+        this.packages.push({
+          dependencyType: depType,
+          name: dependency.packageName,
+          requestedVersion: parsedVersion,
+          lockfileVersion: parsedVersion,
+        });
+      }
+    }
   }
+}
+
+export interface PomXML {
+  // @todo: cut down this interface to simplify it
+  project: {
+    $: {
+      xmlns: string;
+      'xmlns:xsi': string;
+      'xsi:schemaLocation': string;
+    };
+    modelVersion: string;
+    parent: {};
+    groupId: string;
+    artifactId: string;
+    version: string;
+    name: string;
+    description: string;
+    properties: {};
+    dependencies: [
+      {
+        dependency: [
+          {
+            groupId: [string];
+            artifactId: [string];
+            version: [string];
+            scope?: [string];
+            exclusions?: [string];
+          },
+        ];
+      },
+    ];
+    build: {};
+  };
+}
+
+export interface ParsedDependency {
+  packageName: string;
+  version: string;
 }
