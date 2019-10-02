673318515fbb517435284ddad05924cac3686bcc
Jan Vlnas <git@jan.vlnas.cz>
2019-10-02 14:26:47 +0200

    feat(cli): add update-notifier
    Fixes #70

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { Scanner } from './scanner/Scanner';
 import { Command, flags } from '@oclif/command';
 import cli from 'cli-ux';
 import { ServiceError } from './lib/errors';
+import pkg from 'pjson';
+import updateNotifier from 'update-notifier';
 
 class DXScannerCommand extends Command {
   static description = 'Scan your project for possible DX recommendations.';
@@ -26,6 +28,8 @@ class DXScannerCommand extends Command {
     const { args, flags } = this.parse(DXScannerCommand);
     let authorization = flags.authorization ? flags.authorization : undefined;
     const json = flags.json ? flags.json : undefined;
+
+    const notifier = updateNotifier({ pkg });
 
     // const name = flags.name || 'world';
     // this.log(`hello ${name} from ./src/index.ts`);
@@ -53,8 +57,8 @@ class DXScannerCommand extends Command {
         throw error;
       }
     }
-
     cli.action.stop();
+    notifier.notify();
   }
 }
 
