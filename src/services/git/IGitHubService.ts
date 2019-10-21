1f196a2be4bb7d1a641a14fdcfb43116270e68ed
Adela Homolova <adela.homolova@applifting.cz>
2019-10-21 17:16:33 +0200

    fix: add file GitHubService

---
+++ src/services/git/IGitHubService.ts
@@ -0,0 +1,11 @@
+export enum GitHubPullRequestState {
+  open = 'open',
+  closed = 'closed',
+  all = 'all',
+}
+
+export enum GitHubIssueState {
+  open = 'open',
+  closed = 'closed',
+  all = 'all',
+}
