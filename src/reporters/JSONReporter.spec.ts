e592c0e2ae83b57501d3cdd209151b0eaa3e7d04
Prokop Simek <prokop.simek@dxheroes.io>
2019-10-24 16:19:07 +0200

    chore: lint all

--- src/reporters/JSONReporter.spec.ts
+++ src/reporters/JSONReporter.spec.ts
@@ -1,6 +1,6 @@
-import { JSONReporter } from "./JSONReporter";
-import { practiceWithContextFactory } from "../../test/factories/PracticeWithContextFactory";
-import { PracticeEvaluationResult } from "../model";
+import { JSONReporter } from './JSONReporter';
+import { practiceWithContextFactory } from '../../test/factories/PracticeWithContextFactory';
+import { PracticeEvaluationResult } from '../model';
 
 describe('JSONReporter', () => {
   const practicingHighImpactPracticeWithCtx = practiceWithContextFactory();
@@ -8,23 +8,21 @@ describe('JSONReporter', () => {
 
   describe('#report', () => {
     it('one practicing practice', () => {
-      const result = new JSONReporter({uri: "."}).report([practicingHighImpactPracticeWithCtx]);
+      const result = new JSONReporter({ uri: '.' }).report([practicingHighImpactPracticeWithCtx]);
 
-      expect(result).toHaveProperty("components");
-      expect(result).toHaveProperty("uri");
+      expect(result).toHaveProperty('components');
+      expect(result).toHaveProperty('uri');
       expect(result.components).toHaveLength(1);
       expect(result.components[0].practices).toHaveLength(1);
     });
 
     it('one practicing practice and one not practicing', () => {
-      const result = new JSONReporter({uri: "."}).report([practicingHighImpactPracticeWithCtx, notPracticingHighImpactPracticeWithCtx]);
+      const result = new JSONReporter({ uri: '.' }).report([practicingHighImpactPracticeWithCtx, notPracticingHighImpactPracticeWithCtx]);
 
-      expect(result).toHaveProperty("components");
-      expect(result).toHaveProperty("uri");
+      expect(result).toHaveProperty('components');
+      expect(result).toHaveProperty('uri');
       expect(result.components).toHaveLength(1);
       expect(result.components[0].practices).toHaveLength(2);
     });
-
   });
 });
-
