14d6d39a483fd0502365e22eaa0aaf7887a8ca83
Adela Homolova <adela.homolova@applifting.cz>
2019-10-30 08:17:38 +0100

    WIP: tests for fail after report

--- src/scanner/Scanner.spec.ts
+++ src/scanner/Scanner.spec.ts
@@ -1,16 +1,31 @@
-import { createRootContainer } from '../inversify.config';
+import { createRootContainer, createTestContainer, TestContainerContext } from '../inversify.config';
 import { Container } from 'inversify';
 import { Scanner } from './Scanner';
+import { practiceWithContextFactory } from '../../test/factories/PracticeWithContextFactory';
+import { PracticeEvaluationResult } from '../model';
 
 describe('Scanner', () => {
   let rootContainer: Container;
+  let containerCtx: TestContainerContext;
+
   beforeEach(() => {
     rootContainer = createRootContainer({ uri: '.' });
+  });
+
+  beforeAll(async () => {
+    containerCtx = createTestContainer();
   });
 
   it('Can be instantiated from container', () => {
     const scanner = rootContainer.get(Scanner);
     expect(scanner).toBeDefined();
+  });
+
+  it('Report correctly', async () => {
+    const notPracticingHighImpactPracticeWithCtx = practiceWithContextFactory({ evaluation: PracticeEvaluationResult.notPracticing });
+    const notPracticingPracticesToFail = reportArguments.filter(
+      (practice) => practice.evaluation === PracticeEvaluationResult.notPracticing && practice.impact === 'high',
+    );
   });
 
   /*
@@ -19,3 +34,30 @@ describe('Scanner', () => {
     (Scanner class is a integration point)
   */
 });
+
+const reportArguments = [
+  {
+    component: {
+      framework: 'UNKNOWN',
+      language: 'JavaScript',
+      path: '/var/folders/w2/1gzmv1n51bs2z13yd5558ny80000gn/T/dx-scannerIrp1iV',
+      platform: 'UNKNOWN',
+      repositoryPath: 'https://github.com/DXHeroes/empty-js',
+      type: 'Library',
+    },
+    practice: {
+      id: 'LanguageIndependent.GitignoreIsPresent',
+      name: 'Having a .gitignore',
+      impact: 'high',
+      suggestion:
+        'Add gitignore which allow you to ignore files, such as editor backup files, build products or local configuration overrides that you never want to commit into a repository.',
+      reportOnlyOnce: true,
+      url: 'https://git-scm.com/docs/gitignore',
+      defaultImpact: 'high',
+      matcher: ['JsGitignoreIsPresentPractice'],
+    },
+    evaluation: 'practicing',
+    impact: 'high',
+    isOn: true,
+  },
+];
