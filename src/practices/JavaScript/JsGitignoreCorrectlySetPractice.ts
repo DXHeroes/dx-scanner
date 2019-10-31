1910bd24b51ce96462f0cc7d8f601e45dfdace3c
Prokop Simek <prokop.simek@dxheroes.io>
2019-10-31 14:30:51 +0100

    chore: change scripts to patterns as is common

--- src/practices/JavaScript/JsGitignoreCorrectlySetPractice.ts
+++ src/practices/JavaScript/JsGitignoreCorrectlySetPractice.ts
@@ -7,7 +7,7 @@ import { PracticeContext } from '../../contexts/practice/PracticeContext';
   id: 'JavaScript.GitignoreCorrectlySet',
   name: 'Set .gitignore Correctly',
   impact: PracticeImpact.high,
-  suggestion: 'Set scripts in the .gitignore as usual.',
+  suggestion: 'Set patterns in the .gitignore as usual.',
   reportOnlyOnce: true,
   url: 'https://github.com/github/gitignore/blob/master/Node.gitignore',
   dependsOn: { practicing: ['LanguageIndependent.GitignoreIsPresent'] },
