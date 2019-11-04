c5d0586d98f3142b52d0892e2229779f50715b75
ryzzaki <cuong.nugi@gmail.com>
2019-11-04 16:25:11 +0100

    Improved gitignore practice for Java & Maven & Gradle

--- src/practices/Java/JavaGitignoreCorrectlySetPractice.ts
+++ src/practices/Java/JavaGitignoreCorrectlySetPractice.ts
@@ -9,7 +9,7 @@ import { PracticeContext } from '../../contexts/practice/PracticeContext';
   impact: PracticeImpact.high,
   suggestion: 'Scripts in the .gitignore set as usual.',
   reportOnlyOnce: true,
-  url: 'https://github.com/github/gitignore/blob/master/Node.gitignore',
+  url: 'https://github.com/github/gitignore/blob/master/Java.gitignore',
   //dependsOn: { practicing: ['LanguageIndependent.GitignoreIsPresent'] },
 })
 export class JavaGitignoreCorrectlySetPractice implements IPractice {
@@ -32,6 +32,42 @@ export class JavaGitignoreCorrectlySetPractice implements IPractice {
     const content = await ctx.fileInspector.readFile('.gitignore');
     const parsedGitignore = parseGitignore(content);
 
+    const compiledClassRegex = parsedGitignore.find((value: string) => /\*\.class/.test(value));
+    const logRegex = parsedGitignore.find((value: string) => /\*\.log/.test(value));
+    const blueJRegex = parsedGitignore.find((value: string) => /\*\.ctxt/.test(value));
+    const jarRegex = parsedGitignore.find((value: string) => /\*\.jar/.test(value));
+    const warRegex = parsedGitignore.find((value: string) => /\*\.war/.test(value));
+    const narRegex = parsedGitignore.find((value: string) => /\*\.nar/.test(value));
+    const earRegex = parsedGitignore.find((value: string) => /\*\.ear/.test(value));
+    const zipRegex = parsedGitignore.find((value: string) => /\*\.zip/.test(value));
+    const tarRegex = parsedGitignore.find((value: string) => /\*\.tar\.gz/.test(value));
+    const rarRegex = parsedGitignore.find((value: string) => /\*\.rar/.test(value));
+    const mobileToolsRegex = parsedGitignore.find((value: string) => /\.mtj\.tmp/.test(value));
+    const vmCrashLogsRegex = parsedGitignore.find((value: string) => /hs_err_pid*/.test(value));
+
+    const javaGitignore = () => {
+      if (
+        compiledClassRegex &&
+        logRegex &&
+        blueJRegex &&
+        jarRegex &&
+        warRegex &&
+        narRegex &&
+        earRegex &&
+        zipRegex &&
+        tarRegex &&
+        rarRegex &&
+        mobileToolsRegex &&
+        vmCrashLogsRegex
+      ) {
+        return true;
+      }
+    };
+
+    if (!javaGitignore) {
+      return PracticeEvaluationResult.notPracticing;
+    }
+
     if (await ctx.fileInspector.exists('pom.xml')) {
       const mvnRegex = parsedGitignore.find((value: string) => /\.mvn/.test(value));
       const buildNumberRegex = parsedGitignore.find((value: string) => /buildNumber/.test(value));
@@ -44,6 +80,7 @@ export class JavaGitignoreCorrectlySetPractice implements IPractice {
       const releaseRegex = parsedGitignore.find((value: string) => /release\.properties/.test(value));
 
       if (
+        javaGitignore &&
         mvnRegex &&
         buildNumberRegex &&
         dependencyRegex &&
@@ -62,7 +99,7 @@ export class JavaGitignoreCorrectlySetPractice implements IPractice {
       const gradleWrapperRegex = parsedGitignore.find((value: string) => /!gradle-wrapper\.jar/.test(value));
       const taskNameCacheRegex = parsedGitignore.find((value: string) => /\.gradletasknamecache/.test(value));
 
-      if (gradleRegex && gradleAppRegex && gradleWrapperRegex && taskNameCacheRegex) {
+      if (javaGitignore && gradleRegex && gradleAppRegex && gradleWrapperRegex && taskNameCacheRegex) {
         return PracticeEvaluationResult.practicing;
       }
     }
