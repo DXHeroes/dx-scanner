869c21ae76c845cd8ac8410bac0f51342087a693
Adela Homolova <adela.homolova@applifting.cz>
2019-11-20 16:53:46 +0100

    fix: add type of mocked response and fix the response

--- src/services/git/__MOCKS__/bitbucketServiceMockFolder/getRepoCommits.ts
+++ src/services/git/__MOCKS__/bitbucketServiceMockFolder/getRepoCommits.ts
@@ -1,28 +1,32 @@
+import { Paginated } from '../../../../inspectors/common/Paginated';
+
+import { PullCommits } from '../../model';
+//: Paginated<PullCommits>
 export const getRepoCommits = {
   items: [
     {
       sha: 'f9c2cfcfaafa644dcc286ce2fc8b3386d46c11df',
-      url: 'https://bitbucket.org/pypy/pypy/commits/f9c2cfcfaafa644dcc286ce2fc8b3386d46c11df',
-      message:
-        'This checkin might win the prize for the highest amount of XXXs/lines of code:\n' +
-        'it needs a deep review, please :)\n' +
-        '\n' +
-        'Fix W_ExtensionFunction_call_varargs to use the updated calling convention,\n' +
-        'and implement ctx_Arg_Parse.',
-      author: {
-        name: 'antocuni',
-        email: undefined,
-        date: '2019-11-19T10:48:09+00:00',
-      },
-      tree: [
-        [
+      commit: {
+        url: 'https://bitbucket.org/pypy/pypy/commits/f9c2cfcfaafa644dcc286ce2fc8b3386d46c11df',
+        message:
+          'This checkin might win the prize for the highest amount of XXXs/lines of code:\n' +
+          'it needs a deep review, please :)\n' +
+          '\n' +
+          'Fix W_ExtensionFunction_call_varargs to use the updated calling convention,\n' +
+          'and implement ctx_Arg_Parse.',
+        author: {
+          name: 'antocuni',
+          email: 'undefined',
+          date: '2019-11-19T10:48:09+00:00',
+        },
+        tree: [
           {
             sha: '5c9a3fd99fc743f49aaad30952397ab34ad4f40b',
             url: 'https://bitbucket.org/pypy/pypy/commits/5c9a3fd99fc743f49aaad30952397ab34ad4f40b',
           },
         ],
-      ],
-      verified: null,
+        verified: false,
+      },
     },
   ],
   totalCount: 1,
