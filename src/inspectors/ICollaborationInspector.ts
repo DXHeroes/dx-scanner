dccf3ebe6efb21816503ddebb51ebf8dca6ac850
Adela Homolova <adela.homolova@applifting.cz>
2019-11-20 13:05:37 +0100

    feat: add getRepoCommits into CollaborationInspector

--- src/inspectors/ICollaborationInspector.ts
+++ src/inspectors/ICollaborationInspector.ts
@@ -1,11 +1,12 @@
 import { Paginated } from './common/Paginated';
-import { PullCommits, PullFiles, PullRequest } from '../services/git/model';
+import { PullCommits, PullFiles, PullRequest, Commit } from '../services/git/model';
 
 export interface ICollaborationInspector {
   getPullRequests(owner: string, repo: string): Promise<Paginated<PullRequest>>;
   getPullRequest(owner: string, repo: string, prNumber: number): Promise<PullRequest>;
   getPullCommits(owner: string, repo: string, prNumber: number): Promise<Paginated<PullCommits>>;
   getPullRequestFiles(owner: string, repo: string, prNumber: number): Promise<Paginated<PullFiles>>;
+  getRepoCommits(owner: string, repo: string, sha?: string): Promise<Paginated<Commit>>;
 }
 
 export enum PullRequestState {
