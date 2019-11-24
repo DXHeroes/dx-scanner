88edb72943f8c2889bf0a654f3c162b08f859819
ryzzaki <cuong.nugi@gmail.com>
2019-11-24 14:32:37 +0000

    fix(cli): added an action start and stop to --init

--- src/init.configurator.ts
+++ src/init.configurator.ts
@@ -3,14 +3,17 @@ import { Container } from 'inversify';
 import { Types } from './types';
 import { PracticeMetadata } from './model';
 import { ErrorFactory } from './lib/errors';
+import { cli } from 'cli-ux';
 
 export const initialize = (scanPath: string, container: Container) => {
+  cli.action.start(`Initializing configuration: ${scanPath}`);
   const yamlPath = `${scanPath}/.dxscannerrc.yaml`;
   // check if .dxscannerrc.yaml already exists
   const yamlExists: boolean = fs.existsSync(yamlPath);
   if (!yamlExists) {
     createYAML(yamlPath, container);
   }
+  cli.action.stop();
 };
 
 const createYAML = (yamlPath: string, container: Container) => {
