fb46b95172576a5d7d4b9f05d25b07beb9a438d0
ryzzaki <cuong.nugi@gmail.com>
2019-11-25 14:13:53 +0000

    fix(cli): using sync method to write files & practices are sorted alphabetically

--- src/init.configurator.ts
+++ src/init.configurator.ts
@@ -1,13 +1,12 @@
 import * as fs from 'fs';
 import { Container } from 'inversify';
 import { Types } from './types';
-import { PracticeMetadata } from './model';
-import { ErrorFactory } from './lib/errors';
 import { cli } from 'cli-ux';
+import { IPracticeWithMetadata } from './practices/DxPracticeDecorator';
 
 export const initialize = (scanPath: string, container: Container) => {
-  cli.action.start(`Initializing configuration: ${scanPath}`);
   const yamlPath = `${scanPath}/.dxscannerrc.yaml`;
+  cli.action.start(`Initializing configuration: ${yamlPath}`);
   // check if .dxscannerrc.yaml already exists
   const yamlExists: boolean = fs.existsSync(yamlPath);
   if (!yamlExists) {
@@ -19,18 +18,13 @@ export const initialize = (scanPath: string, container: Container) => {
 
 const createYAML = (yamlPath: string, container: Container) => {
   let yamlInitContent = `# practices:`;
-  const initializedPractices: ContainerPracticeMetadata[] = container.getAll(Types.Practice);
-  for (const practice of initializedPractices) {
+  // get Metadata and sort it alphabetically using id
+  const sortedInitializedPractices = container
+    .getAll<IPracticeWithMetadata>(Types.Practice)
+    .sort((a, b) => a.getMetadata().id.localeCompare(b.getMetadata().id));
+  for (const practice of sortedInitializedPractices) {
     const dataObject = practice.getMetadata();
     yamlInitContent += `\n#    ${dataObject.id}: ${dataObject.impact}`;
   }
-  fs.writeFile(yamlPath, yamlInitContent, (err) => {
-    if (err) {
-      throw ErrorFactory.newInternalError(err.message);
-    }
-  });
+  fs.writeFileSync(yamlPath, yamlInitContent);
 };
-
-export interface ContainerPracticeMetadata {
-  getMetadata(): PracticeMetadata;
-}
