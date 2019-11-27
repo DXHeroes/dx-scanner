741e491dd96f844c22b3d7aa5a6701cee4c18c57
Adela Homolova <adela.homolova@applifting.cz>
2019-11-27 08:59:41 +0100

    fix: revert change

--- src/services/git/__MOCKS__/gitHubServiceMockFolder/getPullServiceResponse.mock.ts
+++ src/services/git/__MOCKS__/gitHubServiceMockFolder/getPullServiceResponse.mock.ts
@@ -19,7 +19,7 @@ export const getPullServiceResponse: PullRequest = {
   id: 1,
   mergedAt: '2011-01-26T19:01:12Z',
   state: 'closed',
-  updatedAt: '2011-01-26T19:01:12Z',
+  updatedAt: '2012-03-06T23:06:50Z',
   url: 'https://api.github.com/repos/octocat/Hello-World/pulls/1',
   user: { id: '583231', login: 'octocat', url: 'https://api.github.com/users/octocat' },
 };
