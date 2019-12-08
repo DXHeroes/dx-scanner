af50b8db18612800a28a91260440ee207ce66b2f
Adela Homolova <adela.homolova@applifting.cz>
2019-12-08 16:53:27 +0100

    fix: path

--- src/practices/index.ts
+++ src/practices/index.ts
@@ -23,7 +23,7 @@ import { TsGitignoreCorrectlySetPractice } from './TypeScript/TsGitignoreCorrect
 import { DoesPullRequestsPractice } from './LanguageIndependent/DoesPullRequests';
 import { DependenciesVersionMinorPatchLevel } from './JavaScript/DependenciesVersionMinorPatchLevel';
 import { CorrectCommitMessagesPractice } from './LanguageIndependent/CorrectCommitMessagesPractice';
-import { TimeToSolvePullRequestsPractice } from './LanguageIndependent/TimeToSolvePractice';
+import { TimeToSolvePullRequestsPractice } from './LanguageIndependent/TimeToSolvePullRequestsPractice';
 
 // register practices here
 export const practices = [
