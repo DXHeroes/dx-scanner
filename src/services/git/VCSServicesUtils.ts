98a677b414c8a774e7a4dd039bdbc2839777b539
Adela Homolova <adela.homolova@applifting.cz>
2020-01-10 15:26:53 +0100

    feat: implement getIssueState()

--- src/services/git/VCSServicesUtils.ts
+++ src/services/git/VCSServicesUtils.ts
@@ -1,6 +1,8 @@
 import { PullRequestState } from '../../inspectors/ICollaborationInspector';
-import { BitbucketPullRequestState, VCSServiceType } from './IVCSService';
-import { GitHubPullRequestState } from './IGitHubService';
+import { VCSServiceType } from './IVCSService';
+import { GitHubPullRequestState, GitHubIssueState } from './IGitHubService';
+import { IssueState } from '../../inspectors';
+import { BitbucketPullRequestState, BitbucketIssueState } from '../bitbucket/IBitbucketService';
 
 export class VCSServicesUtils {
   static getPRState = (state: PullRequestState | undefined, service: VCSServiceType) => {
@@ -25,6 +27,34 @@ export class VCSServicesUtils {
           return BitbucketPullRequestState.closed;
         case PullRequestState.all:
           return [BitbucketPullRequestState.open, BitbucketPullRequestState.closed, BitbucketPullRequestState.declined];
+        default:
+          return undefined;
+      }
+    }
+  };
+
+  static getIssueState = (state: IssueState | undefined, service: VCSServiceType) => {
+    if (service === VCSServiceType.github) {
+      switch (state) {
+        case IssueState.open:
+          return GitHubIssueState.open;
+        case IssueState.closed:
+          return GitHubIssueState.closed;
+        case IssueState.all:
+          return GitHubIssueState.all;
+        default:
+          return undefined;
+      }
+    }
+
+    if (service === VCSServiceType.bitbucket) {
+      switch (state) {
+        case IssueState.open:
+          return `"${BitbucketIssueState.new}"`;
+        case IssueState.closed:
+          return `"${BitbucketIssueState.resolved}"`;
+        case IssueState.all:
+          return [`"${BitbucketIssueState.new}"`, `"${BitbucketIssueState.resolved}"`, `"${BitbucketIssueState.closed}"`];
         default:
           return undefined;
       }
