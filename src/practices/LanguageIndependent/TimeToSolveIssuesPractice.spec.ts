295f19620892afda40d871e9978047c9b7b79c84
Adela Homolova <adela.homolova@applifting.cz>
2020-01-10 15:52:56 +0100

    fix: import from right files

--- src/practices/LanguageIndependent/TimeToSolveIssuesPractice.spec.ts
+++ src/practices/LanguageIndependent/TimeToSolveIssuesPractice.spec.ts
@@ -3,11 +3,12 @@ import nock from 'nock';
 import { IssueTrackingInspector } from '../../inspectors';
 import { createTestContainer, TestContainerContext } from '../../inversify.config';
 import { PracticeEvaluationResult } from '../../model';
-import { BitbucketPullRequestState, BitbucketService } from '../../services';
+import { BitbucketService } from '../../services';
 import { getIssuesResponse } from '../../services/git/__MOCKS__/bitbucketServiceMockFolder/getIssuesResponse';
 import { Types } from '../../types';
 import { TimeToSolveIssuesPractice } from './TimeToSolveIssuesPractice';
 import { getIssueResponse } from '../../services/git/__MOCKS__/bitbucketServiceMockFolder';
+import { BitbucketPullRequestState } from '../../services/bitbucket/IBitbucketService';
 
 describe('TimeToSolveIssuesPractice', () => {
   let practice: TimeToSolveIssuesPractice;
