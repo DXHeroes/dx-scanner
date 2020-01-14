9eea4057d09d4068c0468e3e78e3341411f8c45b
Adela Homolova <adela.homolova@applifting.cz>
2020-01-14 13:39:56 +0100

    test: add tests for JavaGitignoreCorrectlySetPractice to increase code coverage

--- src/practices/Java/JavaGitignoreCorrectlySetPractice.spec.ts
+++ src/practices/Java/JavaGitignoreCorrectlySetPractice.spec.ts
@@ -1,5 +1,5 @@
 import { JavaGitignoreCorrectlySetPractice } from './JavaGitignoreCorrectlySetPractice';
-import { PracticeEvaluationResult } from '../../model';
+import { PracticeEvaluationResult, ProgrammingLanguage } from '../../model';
 import { TestContainerContext, createTestContainer } from '../../inversify.config';
 import { pomXMLContents } from '../../detectors/__MOCKS__/Java/pomXMLContents.mock';
 import { buildGRADLEContents } from '../../detectors/__MOCKS__/Java/buildGRADLEContents.mock';
@@ -52,5 +52,53 @@ describe('JavaGitignoreCorrectlySetPractice', () => {
   it('Returns unknown if there is no fileInspector', async () => {
     const evaluated = await practice.evaluate({ ...containerCtx.practiceContext, fileInspector: undefined });
     expect(evaluated).toEqual(PracticeEvaluationResult.unknown);
+  });
+
+  it('Returns unknown if there is *.class, *.log, *.jar, *.war in .gitignore but not correctly set for build.gradle', async () => {
+    containerCtx.virtualFileSystemService.setFileSystem({
+      '.gitignore': `
+      *.class
+      *.log
+      *.jar
+      *.war`,
+      'build.gradle': buildGRADLEContents,
+    });
+
+    const evaluated = await practice.evaluate(containerCtx.practiceContext);
+    expect(evaluated).toEqual(PracticeEvaluationResult.unknown);
+  });
+
+  it('Returns unknown if there is *.class, *.log, *.jar, *.war in .gitignore but not correctly set for build.gradle', async () => {
+    containerCtx.virtualFileSystemService.setFileSystem({
+      '.gitignore': `
+      *.class
+      *.log
+      *.jar
+      *.war`,
+      'pom.xml': pomXMLContents,
+    });
+
+    const evaluated = await practice.evaluate(containerCtx.practiceContext);
+    expect(evaluated).toEqual(PracticeEvaluationResult.unknown);
+  });
+
+  it('Returns unknown if there is correctly set .gitignore, but no pom.xml and build.gradle ', async () => {
+    containerCtx.virtualFileSystemService.setFileSystem({
+      '.gitignore': gitignoreContent,
+    });
+    const evaluated = await practice.evaluate(containerCtx.practiceContext);
+    expect(evaluated).toEqual(PracticeEvaluationResult.unknown);
+  });
+
+  it('Returns true if language is a Java', async () => {
+    containerCtx.practiceContext.projectComponent.language = ProgrammingLanguage.Java;
+    const isApplicable = await practice.isApplicable(containerCtx.practiceContext);
+    expect(isApplicable).toBe(true);
+  });
+
+  it('Returns false if language is not a Java', async () => {
+    containerCtx.practiceContext.projectComponent.language = ProgrammingLanguage.UNKNOWN;
+    const isApplicable = await practice.isApplicable(containerCtx.practiceContext);
+    expect(isApplicable).toBe(false);
   });
 });
