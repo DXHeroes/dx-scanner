457eb72426d4f4ccca135f43e599ea577c4f0077
Adela Homolova <adela.homolova@applifting.cz>
2020-01-21 12:14:13 +0100

    fix: allow fail and recursive to be optional

--- src/scanner/ArgumentsProvider.ts
+++ src/scanner/ArgumentsProvider.ts
@@ -3,8 +3,8 @@ import { PracticeImpact } from '../model';
 export type ArgumentsProvider = {
   uri: string;
   auth: string | undefined;
-  json: boolean | undefined;
-  fail: PracticeImpact | 'all';
-  recursive: boolean;
+  json: boolean;
+  fail?: PracticeImpact | 'all'; // optional as the AP is used also with command dxs practices
+  recursive?: boolean; // optional as the AP is used also with command dxs practices
   ci: boolean;
 };
