14633098c4cd727f97125b7e61f226129f770050
Adela Homolova <adela.homolova@applifting.cz>
2020-01-22 14:55:15 +0100

    test: add test for sortAlphabetically()

--- src/scanner/ScannerUtils.spec.ts
+++ src/scanner/ScannerUtils.spec.ts
@@ -46,6 +46,25 @@ describe('ScannerUtils', () => {
       const practice = ScannerUtils.initPracticeWithMetadata(InvalidTestPractice);
       expect(() => ScannerUtils.sortPractices([practice])).toThrow(expectedErrMsg);
     });
+
+    it('sorts practices alphabetically', () => {
+      const practices = [FirstTestPractice, SecondTestPractice, DeprecatedTSLintPractice, ESLintUsedPractice, TypeScriptUsedPractice].map(
+        ScannerUtils.initPracticeWithMetadata,
+      );
+      const result = ScannerUtils.sortAlphabetically(practices);
+      const id = result.map((practice) => {
+        return practice.getMetadata().id;
+      });
+
+      expect(result).toHaveLength(5);
+      expect(id).toEqual([
+        'JavaScript.DeprecatedTSLint',
+        'JavaScript.ESLintUsed',
+        'JavaScript.TypeScriptUsed',
+        'Mock.FirstTestPractice',
+        'Mock.SecondTestPractice',
+      ]);
+    });
   });
 
   describe('#isFulfilled', () => {
