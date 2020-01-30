3993010a0e6e5a397c920ef0489a6fc43b37b486
Prokop Simek <prokop.simek@dxheroes.io>
2020-01-30 15:10:12 +0100

    revert: add changes for list of practices

--- src/scanner/ArgumentsProvider.ts
+++ src/scanner/ArgumentsProvider.ts
@@ -5,8 +5,8 @@ export type ArgumentsProvider = {
   auth: string | undefined;
   json: boolean;
   details: boolean;
-  fail: PracticeImpact | 'all'; // optional as the AP is used also with command dxs practices
-  recursive: boolean; // optional as the AP is used also with command dxs practices
+  fail: PracticeImpact | 'all';
+  recursive: boolean;
   ci: boolean;
   fix: boolean;
   fixPattern: string | undefined;
