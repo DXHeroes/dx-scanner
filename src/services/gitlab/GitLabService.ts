8122415d32256e345a97072cd788c6604e3f9a31
Adela Homolova <adela.homolova@applifting.cz>
2020-02-17 14:56:58 +0100

    feat: implement getPagination(); return customized interface of PRs

--- src/services/gitlab/GitLabService.ts
+++ src/services/gitlab/GitLabService.ts
@@ -13,6 +13,7 @@ import { Sudo } from 'gitlab/dist/types/core/infrastructure';
 import { ListGetterOptions, PullRequestState, Paginated } from '../../inspectors';
 import { PullRequest } from '../git/model';
 import { VCSServicesUtils } from '../git/VCSServicesUtils';
+import _ from 'lodash';
 
 const debug = Debug('cli:services:git:bitbucket-service');
 
@@ -22,8 +23,10 @@ export class GitLabService {
   private readonly client: Gitlab;
   private cache: ICache;
   private callCount = 0;
+  private readonly argumentsProvider: ArgumentsProvider;
 
   constructor(@inject(Types.ArgumentsProvider) argumentsProvider: ArgumentsProvider) {
+    this.argumentsProvider = argumentsProvider;
     const parsedUrl = GitServiceUtils.parseGitlabUrl(argumentsProvider.uri);
 
     this.cache = new InMemoryCache();
@@ -46,19 +49,75 @@ export class GitLabService {
     owner: string,
     repo: string,
     options?: { withDiffStat?: boolean } & ListGetterOptions<{ state?: PullRequestState }>,
-    //: Promise<Paginated<PullRequest>>
-  ) {
+  ): Promise<Paginated<PullRequest>> {
     const state = VCSServicesUtils.getGitLabPRState(options?.filter?.state);
 
-    const response = await this.client.MergeRequests.all({
+    const { data, pagination } = <any>await this.client.MergeRequests.all({
       page: options?.pagination?.page,
       per_page: options?.pagination?.perPage,
+      showPagination: true,
+
       projectId: `${owner}/${repo}`,
       state,
     });
 
-    console.log(response);
-    return;
+    const parsedUrl = GitServiceUtils.parseGitlabUrl(this.argumentsProvider.uri);
+    const repoUrl = `${parsedUrl.host}/${owner}/${repo}`;
+    let ownerInfo = (<any>await this.client.Users.all({ username: owner }))[0];
+    if (!ownerInfo) {
+      ownerInfo = <any>await this.client.Groups.show(owner);
+    }
+
+    const items = <PullRequest[]>await Promise.all(
+      data.map((val: any) => {
+        const pullRequest = {
+          user: {
+            id: val.author.id,
+            login: val.author.username,
+            url: val.author.web_url,
+          },
+          url: val.web_url,
+          body: val.desciption,
+          createdAt: val.created_at,
+          updatedAt: val.updated_at,
+          closedAt: val.closed_at,
+          state: val.state,
+          id: val.id, //iid?
+          base: {
+            repo: {
+              url: repoUrl,
+              name: repo,
+              id: val.project_id,
+              owner: {
+                url: `${parsedUrl.host}/${owner}`,
+                id: ownerInfo.id,
+                login: ownerInfo.username,
+              },
+            },
+          },
+        };
+        // Get number of changes, additions and deletions in PullRequest if the withDiffStat is true
+        if (options?.withDiffStat) {
+          //TODO
+          // https://gitlab.com/gitlab-org/gitlab/issues/206904
+          //const lines = await this.getPullsDiffStat(owner, repo, val.number);
+          //return { ...pullRequest, lines };
+        }
+        return pullRequest;
+      }),
+    );
+    const customPagination = this.getPagination(pagination);
+
+    return { items, ...customPagination };
+  }
+
+  getPagination(pagination: any) {
+    const hasNextPage = pagination.next;
+    const hasPreviousPage = pagination.previous;
+    const page = pagination.current;
+    const perPage = pagination.perPage;
+
+    return { totalCount: pagination.total, hasNextPage, hasPreviousPage, page, perPage };
   }
 
   /**
