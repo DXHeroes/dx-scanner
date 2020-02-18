3cbe59eabb332d9b72dccac254201e9bb1669fe2
Adela Homolova <adela.homolova@applifting.cz>
2020-02-18 22:21:09 +0100

    feat: implement get() - mergeRequests

--- src/services/gitlab/gitlabClient/resources/MergeRequests.ts
+++ src/services/gitlab/gitlabClient/resources/MergeRequests.ts
@@ -27,6 +27,28 @@ export class MergeRequests extends GitLabClient {
     const response = await this.api.get(endpoint, { params: queryParams });
     return parseResponse(response);
   }
+
+  /**
+   *
+   * @param projectId - The ID or URL path of the project, e.g. DXHeroes/dx-scanner
+   * @param mergeRequestIId
+   * @param options
+   *
+   * Get single pull request (merge request) of given merge_request_iid
+   */
+  async get(projectId: string, mergeRequestIId: number, options?: ListGetterOptions<{ state?: GitLabPullRequestState }>) {
+    const endpoint = `projects/${encodeURIComponent(projectId)}/merge_requests/${mergeRequestIId}`;
+
+    const queryParams = {
+      page: options?.pagination?.page,
+      // eslint-disable-next-line @typescript-eslint/camelcase
+      per_page: options?.pagination?.perPage,
+      state: options?.filter?.state,
+    };
+
+    const response = await this.api.get(endpoint, { params: queryParams });
+    return parseResponse(response);
+  }
 }
 
 //TODO move to another file
