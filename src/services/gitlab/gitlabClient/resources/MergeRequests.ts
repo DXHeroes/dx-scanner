74ff9640610ce1dd798c2cda7aea8f306ce029de
Adela Homolova <adela.homolova@applifting.cz>
2020-02-19 17:18:07 +0100

    feat: add interfaces and type responses

--- src/services/gitlab/gitlabClient/resources/MergeRequests.ts
+++ src/services/gitlab/gitlabClient/resources/MergeRequests.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/camelcase */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { GitLabConstructor } from '../GitLabClient';
-import { parseResponse } from '../Utils';
+import { parseResponse, CustomAxiosResponse } from '../Utils';
 import { ListGetterOptions, PaginationParams } from '../../../../inspectors';
 import { GitLabPullRequestState } from '../../IGitLabService';
+import { AxiosResponse } from 'axios';
+import { User } from './UsersOrGroups';
 
 export class MergeRequests extends GitLabConstructor {
   api = this.createAxiosInstance();
@@ -15,7 +17,10 @@ export class MergeRequests extends GitLabConstructor {
    *
    * List all merge requests for project.
    */
-  async list(projectId: string, options?: ListGetterOptions<{ state?: GitLabPullRequestState }>) {
+  async list(
+    projectId: string,
+    options?: ListGetterOptions<{ state?: GitLabPullRequestState }>,
+  ): Promise<CustomAxiosResponse<MergeRequest[]>> {
     const endpoint = `projects/${encodeURIComponent(projectId)}/merge_requests`;
 
     const params = {
@@ -24,7 +29,7 @@ export class MergeRequests extends GitLabConstructor {
       state: options?.filter?.state,
     };
 
-    const response = await this.api.get(endpoint, { params });
+    const response: AxiosResponse<MergeRequest[]> = await this.api.get(endpoint, { params });
     return parseResponse(response);
   }
 
@@ -36,10 +41,10 @@ export class MergeRequests extends GitLabConstructor {
    *
    * Get single pull request (merge request) of given merge_request_iid
    */
-  async get(projectId: string, mergeRequestIId: number) {
+  async get(projectId: string, mergeRequestIId: number): Promise<CustomAxiosResponse<MergeRequest>> {
     const endpoint = `projects/${encodeURIComponent(projectId)}/merge_requests/${mergeRequestIId}`;
 
-    const response = await this.api.get(endpoint);
+    const response: AxiosResponse<MergeRequest> = await this.api.get(endpoint);
     return parseResponse(response);
   }
 
@@ -51,7 +56,7 @@ export class MergeRequests extends GitLabConstructor {
    *
    * List all commits for merge request of given iid
    */
-  async commits(projectId: string, mergeRequestIId: number, pagination?: PaginationParams) {
+  async commits(projectId: string, mergeRequestIId: number, pagination?: PaginationParams): Promise<CustomAxiosResponse<MergeCommit[]>> {
     const endpoint = `projects/${encodeURIComponent(projectId)}/merge_requests/${mergeRequestIId}/commits`;
 
     const params = {
@@ -59,7 +64,7 @@ export class MergeRequests extends GitLabConstructor {
       per_page: pagination?.perPage,
     };
 
-    const response = await this.api.get(endpoint, { params });
+    const response: AxiosResponse<MergeCommit[]> = await this.api.get(endpoint, { params });
     return parseResponse(response);
   }
 
@@ -71,7 +76,7 @@ export class MergeRequests extends GitLabConstructor {
    *
    * Gets a list of all notes for a single merge request.
    */
-  async comments(projectId: string, mergeRequestIId: number, pagination?: PaginationParams) {
+  async comments(projectId: string, mergeRequestIId: number, pagination?: PaginationParams): Promise<CustomAxiosResponse<MergeComment[]>> {
     const endpoint = `projects/${encodeURIComponent(projectId)}/merge_requests/${mergeRequestIId}/notes`;
 
     const params = {
@@ -79,7 +84,7 @@ export class MergeRequests extends GitLabConstructor {
       per_page: pagination?.perPage,
     };
 
-    const response = await this.api.get(endpoint, { params });
+    const response: AxiosResponse<MergeComment[]> = await this.api.get(endpoint, { params });
     return parseResponse(response);
   }
 
@@ -92,10 +97,10 @@ export class MergeRequests extends GitLabConstructor {
    * Creates a new note for a single merge request.
    * If you create a note where the body only contains an Award Emoji, you’ll receive this object back.
    */
-  async createComment(projectId: string, mergeRequestIId: number, body: string) {
+  async createComment(projectId: string, mergeRequestIId: number, body: string): Promise<CustomAxiosResponse<MergeComment[]>> {
     const endpoint = `projects/${encodeURIComponent(projectId)}/merge_requests/${mergeRequestIId}/notes`;
 
-    const response = await this.api.post(endpoint, { data: body });
+    const response: AxiosResponse<MergeComment[]> = await this.api.post(endpoint, { data: body });
     return parseResponse(response);
   }
 
@@ -106,12 +111,116 @@ export class MergeRequests extends GitLabConstructor {
    * @param body
    * @param commentId
    *
-   * Modify existing note of a merge request.
+   * Modify existing note of a merge request
    */
-  async updateComment(projectId: string, mergeRequestIId: number, body: string, commentId: number) {
+  async updateComment(
+    projectId: string,
+    mergeRequestIId: number,
+    body: string,
+    commentId: number,
+  ): Promise<CustomAxiosResponse<MergeComment[]>> {
     const endpoint = `projects/${encodeURIComponent(projectId)}/merge_requests/${mergeRequestIId}/notes/${commentId}`;
 
-    const response = await this.api.put(endpoint, { data: body });
+    const response: AxiosResponse<MergeComment[]> = await this.api.put(endpoint, { data: body });
     return parseResponse(response);
   }
+}
+
+export interface MergeRequest {
+  id: number;
+  iid: number;
+  project_id: number;
+  title: string;
+  description: string;
+  state: string;
+  created_at: Date;
+  updated_at: Date;
+  merged_by: User;
+  merged_at?: Date;
+  closed_by: User;
+  closed_at?: Date;
+  target_branch: string;
+  source_branch: string;
+  user_notes_count: number;
+  upvotes: number;
+  downvotes: number;
+  assignee: User;
+  author: User;
+  assignees: User[];
+  source_project_id: number;
+  target_project_id: number;
+  labels: string[];
+  work_in_progress: boolean;
+  milestone?: any;
+  merge_when_pipeline_succeeds: boolean;
+  merge_status: string;
+  sha: string;
+  merge_commit_sha: string;
+  squash_commit_sha?: any;
+  discussion_locked?: any;
+  should_remove_source_branch?: boolean;
+  force_remove_source_branch: boolean;
+  reference: string;
+  web_url: string;
+  time_stats: TimeStats;
+  squash: boolean;
+  task_completion_status: TaskCompletionStatus;
+  has_conflicts: boolean;
+  blocking_discussions_resolved: boolean;
+}
+
+export interface MergeCommit {
+  id: string;
+  short_id: string;
+  created_at: Date;
+  parent_ids: any[];
+  title: string;
+  message: string;
+  author_name: string;
+  author_email: string;
+  authored_date: Date;
+  committer_name: string;
+  committer_email: string;
+  committed_date: Date;
+}
+
+export interface MergeComment {
+  id: number;
+  type: string;
+  body: string;
+  attachment?: any;
+  author: User;
+  created_at: Date;
+  updated_at: Date;
+  system: boolean;
+  noteable_id: number;
+  noteable_type: string;
+  resolvable: boolean;
+  noteable_iid: number;
+  position: Position;
+  resolved?: boolean;
+  resolved_by: User;
+}
+
+export interface Position {
+  base_sha: string;
+  start_sha: string;
+  head_sha: string;
+  old_path: string;
+  new_path: string;
+  position_type: string;
+  old_line?: any;
+  new_line: number;
+}
+
+export interface TimeStats {
+  time_estimate: number;
+  total_time_spent: number;
+  human_time_estimate?: any;
+  human_total_time_spent?: any;
+}
+
+export interface TaskCompletionStatus {
+  count: number;
+  completed_count: number;
 }
--- src/services/gitlab/gitlabClient/resources/Projects.ts
+++ src/services/gitlab/gitlabClient/resources/Projects.ts
@@ -1,13 +1,126 @@
 import { GitLabConstructor } from '../GitLabClient';
-import { parseResponse } from '../Utils';
+import { parseResponse, CustomAxiosResponse } from '../Utils';
+import { AxiosResponse } from 'axios';
 
 export class Projects extends GitLabConstructor {
   api = this.createAxiosInstance();
 
-  async get(projectId: string) {
+  async get(projectId: string): Promise<CustomAxiosResponse<Project>> {
     const endpoint = `projects/${encodeURIComponent(projectId)}`;
 
-    const response = await this.api.get(endpoint);
+    const response: AxiosResponse<Project> = await this.api.get(endpoint);
     return parseResponse(response);
   }
+}
+
+export interface Project {
+  id: number;
+  description: string;
+  name: string;
+  name_with_namespace: string;
+  path: string;
+  path_with_namespace: string;
+  created_at: Date;
+  default_branch: string;
+  tag_list: any[];
+  ssh_url_to_repo: string;
+  http_url_to_repo: string;
+  web_url: string;
+  readme_url: string;
+  avatar_url?: any;
+  star_count: number;
+  forks_count: number;
+  last_activity_at: Date;
+  namespace: Namespace;
+  _links: Links;
+  empty_repo: boolean;
+  archived: boolean;
+  visibility: string;
+  resolve_outdated_diff_discussions: boolean;
+  container_registry_enabled: boolean;
+  issues_enabled: boolean;
+  merge_requests_enabled: boolean;
+  wiki_enabled: boolean;
+  jobs_enabled: boolean;
+  snippets_enabled: boolean;
+  issues_access_level: string;
+  repository_access_level: string;
+  merge_requests_access_level: string;
+  wiki_access_level: string;
+  builds_access_level: string;
+  snippets_access_level: string;
+  shared_runners_enabled: boolean;
+  lfs_enabled: boolean;
+  creator_id: number;
+  forked_from_project: ForkedFromProject;
+  import_status: string;
+  open_issues_count: number;
+  ci_default_git_depth?: any;
+  public_jobs: boolean;
+  build_timeout: number;
+  auto_cancel_pending_pipelines: string;
+  build_coverage_regex?: any;
+  ci_config_path?: any;
+  shared_with_groups: any[];
+  only_allow_merge_if_pipeline_succeeds: boolean;
+  request_access_enabled: boolean;
+  only_allow_merge_if_all_discussions_are_resolved: boolean;
+  remove_source_branch_after_merge?: any;
+  printing_merge_request_link_enabled: boolean;
+  merge_method: string;
+  auto_devops_enabled: boolean;
+  auto_devops_deploy_strategy: string;
+  permissions: Permissions;
+}
+
+export interface Namespace {
+  id: number;
+  name: string;
+  path: string;
+  kind: string;
+  full_path: string;
+  parent_id?: any;
+  avatar_url: string;
+  web_url: string;
+}
+
+export interface Links {
+  self: string;
+  issues: string;
+  merge_requests: string;
+  repo_branches: string;
+  labels: string;
+  events: string;
+  members: string;
+}
+
+export interface ForkedFromProject {
+  id: number;
+  description: string;
+  name: string;
+  name_with_namespace: string;
+  path: string;
+  path_with_namespace: string;
+  created_at: Date;
+  default_branch: string;
+  tag_list: any[];
+  ssh_url_to_repo: string;
+  http_url_to_repo: string;
+  web_url: string;
+  readme_url: string;
+  avatar_url?: any;
+  star_count: number;
+  forks_count: number;
+  last_activity_at: Date;
+  namespace: Namespace;
+}
+
+export interface GroupAccess {
+  access_level: number;
+  notification_level: number;
+}
+
+export interface Permissions {
+  project_access?: any;
+  group_access: GroupAccess;
 }
