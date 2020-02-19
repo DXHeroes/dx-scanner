04515dc818926f399a581d8abfff728a1523e9d4
Adela Homolova <adela.homolova@applifting.cz>
2020-02-19 14:22:40 +0100

    feat: implement comments(), createComment(), updateComment()

--- src/services/gitlab/gitlabClient/resources/MergeRequests.ts
+++ src/services/gitlab/gitlabClient/resources/MergeRequests.ts
@@ -62,4 +62,56 @@ export class MergeRequests extends GitLabConstructor {
     const response = await this.api.get(endpoint, { params });
     return parseResponse(response);
   }
+
+  /**
+   *
+   * @param projectId - The ID or URL path of the project, e.g. DXHeroes/dx-scanner
+   * @param mergeRequestIId
+   * @param pagination
+   *
+   * Gets a list of all notes for a single merge request.
+   */
+  async comments(projectId: string, mergeRequestIId: number, pagination?: PaginationParams) {
+    const endpoint = `projects/${encodeURIComponent(projectId)}/merge_requests/${mergeRequestIId}/notes`;
+
+    const params = {
+      page: pagination?.page,
+      per_page: pagination?.perPage,
+    };
+
+    const response = await this.api.get(endpoint, { params });
+    return parseResponse(response);
+  }
+
+  /**
+   *
+   * @param projectId - The ID or URL path of the project, e.g. DXHeroes/dx-scanner
+   * @param mergeRequestIId
+   * @param body
+   *
+   * Creates a new note for a single merge request.
+   * If you create a note where the body only contains an Award Emoji, you’ll receive this object back.
+   */
+  async createComment(projectId: string, mergeRequestIId: number, body: string) {
+    const endpoint = `projects/${encodeURIComponent(projectId)}/merge_requests/${mergeRequestIId}/notes`;
+
+    const response = await this.api.post(endpoint, { data: body });
+    return parseResponse(response);
+  }
+
+  /**
+   *
+   * @param projectId
+   * @param mergeRequestIId
+   * @param body
+   * @param commentId
+   *
+   * Modify existing note of a merge request.
+   */
+  async updateComment(projectId: string, mergeRequestIId: number, body: string, commentId: number) {
+    const endpoint = `projects/${encodeURIComponent(projectId)}/merge_requests/${mergeRequestIId}/notes/${commentId}`;
+
+    const response = await this.api.put(endpoint, { data: body });
+    return parseResponse(response);
+  }
 }
