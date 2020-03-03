2b0ac414992fce6eb115d713cf6b3401ba359b8e
Adela Homolova <adela.homolova@applifting.cz>
2020-03-03 23:20:42 +0100

    test: add test for listPullRequests()

--- src/services/gitlab/GitLabService.spec.ts
+++ src/services/gitlab/GitLabService.spec.ts
@@ -1,18 +1,30 @@
+import { PullRequestState } from '../../inspectors';
 import { argumentsProviderFactory } from '../../test/factories/ArgumentsProviderFactory';
+import { gitLabPullRequestResponseFactory } from '../../test/factories/responses/gitLab/prResponseFactory';
+import { GitLabNock } from '../../test/helpers/gitLabNock';
 import { GitLabService } from './GitLabService';
+import { GitLabPullRequestState } from './IGitLabService';
+import util from 'util';
+import { listPullRequestsResponse } from '../git/__MOCKS__/gitLabServiceMockFolder/listPullRequestsResponse';
 
 describe('GitLab Service', () => {
   let service: GitLabService;
-  //let bitbucketNock: BitbucketNock;
+  let gitLabNock: GitLabNock;
 
   beforeEach(async () => {
-    service = new GitLabService(argumentsProviderFactory({ uri: '.' }));
-
-    //bitbucketNock = new BitbucketNock('pypy', 'pypy');
+    service = new GitLabService(argumentsProviderFactory({ uri: 'https://gitlab.com/gitlab-org/gitlab' }));
+    gitLabNock = new GitLabNock('gitlab-org', 'gitlab');
   });
 
-  it('returns object', async () => {
-    const response = await service.getRepo('caina', 'time-sheet');
-    expect(response).toBeDefined();
+  it('Returns list of merge requests in own interface', async () => {
+    jest.setTimeout(100000);
+
+    const mockPr = gitLabPullRequestResponseFactory();
+
+    gitLabNock.getGroupInfo();
+    gitLabNock.listPullRequestsResponse([mockPr], { filter: { state: GitLabPullRequestState.open } });
+
+    const response = await service.listPullRequests('gitlab-org', 'gitlab', { filter: { state: PullRequestState.open } });
+    expect(response).toMatchObject(listPullRequestsResponse());
   });
 });
