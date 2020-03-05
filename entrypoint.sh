65cabbe1206e9ccb2e2cac436bb8cb28aece3331
Prokop Simek <prokop.simek@dxheroes.io>
2020-03-05 18:45:04 +0100

    ci: add job for docker images builds

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,21 @@
 #!/bin/sh -l
 
-echo "Start DX Scanner Action"
+if [[ $1 == *"bash" ]]; then
+  /bin/bash
+  exit 0;
+elif [[ $1 == *"sh" ]]; then
+  /bin/sh
+  exit 0;
+fi
 
-dx-scanner run --ci -r -a $1
+echo "Start DX Scanner Action"
+if [[ $1 == "dx-scanner "* || $1 == "dxs "* || $1 == "dxscanner "* ]]; then
+  echo $@
+  $@
+elif [[ $1 == "-"* ]]; then
+  echo dx-scanner run $@
+  dx-scanner run $@
+else
+  echo dx-scanner run --ci -r -a $@
+  dx-scanner run --ci -r -a $@
+fi
