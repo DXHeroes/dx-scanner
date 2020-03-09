3ba2d64c9e09b77f67da6e2dbc19e324b92dc6b8
Prokop Simek <prokop.simek@dxheroes.io>
2020-03-09 15:09:04 +0100

    fix: set bash and exec commands instead of raw commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,22 @@
-#!/bin/sh -l
+#!/bin/bash
+set -e
 
 if [[ $1 == *"bash" ]]; then
-  /bin/bash
+  exec "/bin/bash"
   exit 0;
 elif [[ $1 == *"sh" ]]; then
-  /bin/sh
+  exec "/bin/sh"
   exit 0;
 fi
 
 echo "Start DX Scanner Action"
 if [[ $1 == "dx-scanner "* || $1 == "dxs "* || $1 == "dxscanner "* ]]; then
   echo $@
-  $@
+  exec "$@"
 elif [[ $1 == "-"* ]]; then
   echo dx-scanner run $@
-  dx-scanner run $@
+  exec dx-scanner run "$@"
 else
   echo dx-scanner run --ci -r -a $@
-  dx-scanner run --ci -r -a $@
+  exec dx-scanner run --ci -r -a "$@"
 fi
