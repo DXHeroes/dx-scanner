fd23c7421f5592bb0b278e49faef9261301798aa
Adela Homolova <53510747+adelkahomolova@users.noreply.github.com>
2020-03-23 19:47:27 +0100

    fix: add return type
    Co-Authored-By: Prokop Simek <prokopsimek@users.noreply.github.com>

--- src/test/factories/responses/gitLab/versionResponseFactory.ts
+++ src/test/factories/responses/gitLab/versionResponseFactory.ts
@@ -2,6 +2,6 @@
 import _ from 'lodash';
 import { VersionResponse } from '../../../../services/gitlab/gitlabClient/resources/Version';
 
-export const gitLabVersionResponseFactory = (params?: Partial<VersionResponse>) => {
+export const gitLabVersionResponseFactory = (params?: Partial<VersionResponse>): VersionResponse => {
   return _.merge({ version: '1.0.0', revision: '225c2e' }, params);
 };
