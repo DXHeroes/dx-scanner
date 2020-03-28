a8f2bb471e4211f75306c2f85c448f200c05e5cb
Adela Homolova <adela.homolova@applifting.cz>
2020-03-28 18:49:50 +0100

    chore: implement axios.post(), return void

--- src/reporters/ReporterCollectData.ts
+++ src/reporters/ReporterCollectData.ts
@@ -4,6 +4,7 @@ import { ArgumentsProvider } from '../scanner';
 import { Types } from '../types';
 import { PracticeWithContextForReporter, IReporter } from './IReporter';
 import { ProjectComponent } from '../model';
+import Axios from 'axios';
 
 @injectable()
 export class ReporterCollectData implements IReporter {
@@ -13,23 +14,25 @@ export class ReporterCollectData implements IReporter {
     this.argumentsProvider = argumentsProvider;
   }
 
-  async report(practicesAndComponents: PracticeWithContextForReporter[]): Promise<ReportData[]> {
+  async report(practicesAndComponents: PracticeWithContextForReporter[]): Promise<void> {
     const reportData = this.buildReport(practicesAndComponents);
 
-    return reportData;
+    //TODO add endpoint
+    await Axios.post('ADD ENDPOINT', reportData);
   }
 
-  buildReport(practicesAndComponents: PracticeWithContextForReporter[]) {
+  buildReport(practicesAndComponents: PracticeWithContextForReporter[]): ReportData[] {
     const componentsWithPractices = ReporterUtils.getComponentsWithPractices(practicesAndComponents);
 
     const dxScore = ReporterUtils.computeDXScore(practicesAndComponents);
     const componentsWithScore = [];
+
     for (const cwp of componentsWithPractices) {
       const dxScoreForComponent = dxScore.components.find((c) => c.path === cwp.component.path)!.value;
       const componentWithScore = { component: cwp.component, dxScore: dxScoreForComponent };
+
       componentsWithScore.push(componentWithScore);
     }
-    console.log(componentsWithScore);
 
     return componentsWithScore;
   }
