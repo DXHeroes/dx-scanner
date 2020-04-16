4d07446a85772e413beecf59b59f8089fc9d428d
Michal Vlasák <daeatel@gmail.com>
2020-04-16 13:00:32 +0200

    feat(fixer): add gitignore fixer

--- src/practices/LanguageIndependent/GitignoreIsPresentPractice.ts
+++ src/practices/LanguageIndependent/GitignoreIsPresentPractice.ts
@@ -1,7 +1,15 @@
 import { IPractice } from '../IPractice';
-import { PracticeEvaluationResult, PracticeImpact } from '../../model';
+import { PracticeEvaluationResult, PracticeImpact, ProgrammingLanguage } from '../../model';
 import { DxPractice } from '../DxPracticeDecorator';
 import { PracticeContext } from '../../contexts/practice/PracticeContext';
+import { FixerContext } from '../../contexts/fixer/FixerContext';
+import gi from 'gitignore';
+import fs, { unlink } from 'fs';
+import os from 'os';
+import { v4 as uuidv4 } from 'uuid';
+import { promisify } from 'util';
+
+const noop = () => undefined;
 
 @DxPractice({
   id: 'LanguageIndependent.GitignoreIsPresent',
@@ -32,5 +40,35 @@ export class GitignoreIsPresentPractice implements IPractice {
     }
 
     return PracticeEvaluationResult.notPracticing;
+  }
+
+  async fix(ctx: FixerContext) {
+    // do necessary checks
+    const inspector = ctx.fileInspector?.basePath ? ctx.fileInspector : ctx.root.fileInspector;
+    if (!inspector) return;
+    const availableTypes = await promisify(gi.getTypes)();
+    if (!availableTypes) return;
+
+    // get the gitignore type
+    let type: string | undefined;
+    if ([ProgrammingLanguage.JavaScript, ProgrammingLanguage.TypeScript].includes(ctx.projectComponent.language)) {
+      type = 'Node';
+    } else if (availableTypes.includes(ctx.projectComponent.language)) {
+      type = ctx.projectComponent.language;
+    }
+    if (!type) return;
+
+    // download data to tmp file
+    const tmpFilePath = `${os.tmpdir()}/${uuidv4()}`;
+    const writeTo = fs.createWriteStream(tmpFilePath);
+    await new Promise((resolve) => {
+      writeTo.on('finish', resolve); // continue once the write is finished
+      gi.writeFile({ type: type!, file: writeTo }, noop);
+    });
+
+    // move data to gitignore file
+    const data = fs.readFileSync(tmpFilePath, 'utf8');
+    await inspector.writeFile('.gitignore', data);
+    await promisify(unlink)(tmpFilePath);
   }
 }
