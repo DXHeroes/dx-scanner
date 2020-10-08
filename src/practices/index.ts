c47a54ef91f2c8a8ef13374ec137faa5d4af174b
Anish Kargaonkar <anishkargaonkar@gmail.com>
2020-10-08 08:43:47 +0530

    feat: add linter practice for Go

--- src/practices/index.ts
+++ src/practices/index.ts
@@ -39,6 +39,7 @@ import { JavaCodeStyleUsedPractice } from './Java/JavaCodeStyleUsedPractice';
 import { JavaSpecifiedDependencyVersions } from './Java/JavaSpecifiedDependencyVersions';
 import { JavaLog4JConfiguredPractice } from './Java/JavaLog4JConfiguredPractice';
 import { JavaPropertiesUsedPractice } from './Java/JavaPropertiesUsedPractice';
+import { GoLinterUsedPractice } from './Go/GoLinterUsedPractice';
 
 // register practices here
 export const practices = [
@@ -83,4 +84,5 @@ export const practices = [
   JavaSpecifiedDependencyVersions,
   JavaLog4JConfiguredPractice,
   JavaPropertiesUsedPractice,
+  GoLinterUsedPractice,
 ];
