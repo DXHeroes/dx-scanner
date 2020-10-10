f534e3b06690de97aeccec4b66aa5978826710ff
PuZZleDucK <puzzleduck@gmail.com>
2020-10-10 23:19:17 +1100

    ruby package inspector for component detector

---
+++ src/inspectors/package/RubyPackageInspector.spec.ts
@@ -0,0 +1,181 @@
+import { DependencyType } from '../IPackageInspector';
+import { TestContainerContext, createTestContainer } from '../../inversify.config';
+import { DirectoryJSON } from 'memfs/lib/volume';
+import { RubyPackageInspector } from './RubyPackageInspector';
+import { gemfileContents } from '../../detectors/__MOCKS__/Ruby/GemfileContents.mock';
+
+describe('RubyPackageInspector', () => {
+  let inspector: RubyPackageInspector;
+  let containerCtx: TestContainerContext;
+
+  beforeAll(() => {
+    containerCtx = createTestContainer();
+    containerCtx.container.bind('RubyPackageInspector').to(RubyPackageInspector);
+    inspector = containerCtx.container.get('RubyPackageInspector');
+  });
+
+  beforeEach(async () => {
+    containerCtx.container.rebind('RubyPackageInspector').to(RubyPackageInspector);
+    inspector = containerCtx.container.get('RubyPackageInspector');
+
+    containerCtx.virtualFileSystemService.setFileSystem({
+      'Gemfile': gemfileContents,
+    });
+  });
+
+  afterEach(async () => {
+    containerCtx.virtualFileSystemService.clearFileSystem();
+  });
+
+  it('It inits and loads packages', async () => {
+    await inspector.init();
+    expect(inspector.packages!.length).toBeGreaterThan(0);
+  });
+
+  it('Parses normal packages correctly', async () => {
+    await inspector.init();
+    const pkg = inspector.findPackage('after_party');
+
+    if (!pkg) {
+      fail();
+    } else {
+      expect(pkg.name).toEqual('after_party');
+      expect(pkg.dependencyType).toEqual(DependencyType.Runtime);
+    }
+  });
+
+  it('Parses the packages correctly with pessimistic version constraint (~>)', async () => {
+    await inspector.init();
+    const pkg = inspector.findPackage('rails');
+
+    if (!pkg) {
+      fail();
+    } else {
+      expect(pkg.name).toEqual('rails');
+      expect(pkg.dependencyType).toEqual(DependencyType.Runtime);
+      expect(pkg.requestedVersion.value).toEqual('6.0.3');
+      expect(pkg.requestedVersion.major).toEqual('6');
+      expect(pkg.requestedVersion.minor).toEqual('0');
+      expect(pkg.requestedVersion.patch).toEqual('3');
+    }
+  });
+
+  it('Parses the packages correctly with greater than or equal (>=)', async () => {
+    await inspector.init();
+    const pkg = inspector.findPackage('bootsnap');
+
+    if (!pkg) {
+      fail();
+    } else {
+      expect(pkg.name).toEqual('bootsnap');
+      expect(pkg.dependencyType).toEqual(DependencyType.Runtime);
+      expect(pkg.requestedVersion.value).toEqual('1.4.2');
+      expect(pkg.requestedVersion.major).toEqual('1');
+      expect(pkg.requestedVersion.minor).toEqual('4');
+      expect(pkg.requestedVersion.patch).toEqual('2');
+    }
+  });
+
+  it('Parses the packages correctly with multiple constraints (>= and <)', async () => {
+    await inspector.init();
+    const pkg = inspector.findPackage('pg');
+
+    if (!pkg) {
+      fail();
+    } else {
+      expect(pkg.name).toEqual('pg');
+      expect(pkg.dependencyType).toEqual(DependencyType.Runtime);
+      expect(pkg.requestedVersion.value).toEqual('0.18'); //between 0.18 and 2.0
+      expect(pkg.requestedVersion.major).toEqual('0');
+      expect(pkg.requestedVersion.minor).toEqual('18');
+      expect(pkg.requestedVersion.patch).toEqual('0');
+    }
+  });
+
+  it('Parses the packages correctly with version number in comment', async () => {
+    await inspector.init();
+    const pkg = inspector.findPackage('devise');
+
+    if (!pkg) {
+      fail();
+    } else {
+      expect(pkg.name).toEqual('devise');
+      expect(pkg.dependencyType).toEqual(DependencyType.Runtime);
+      expect(pkg.requestedVersion.value).toEqual('0.0.0');
+      expect(pkg.requestedVersion.major).toEqual('0');
+      expect(pkg.requestedVersion.minor).toEqual('0');
+      expect(pkg.requestedVersion.patch).toEqual('0');
+    }
+  });
+
+  describe('functions', () => {
+    describe('#findPackage', () => {
+      beforeEach(async () => {
+        await inspector.init();
+      });
+
+      it('Finds package if it exists', async () => {
+        const pkg = inspector.findPackage('amazing_print');
+        expect(pkg).toBeDefined();
+      });
+
+      it('Returns undefined if the package does not exist', async () => {
+        const pkg = inspector.findPackage('adequite_print');
+        expect(pkg).toBeUndefined();
+      });
+    });
+
+    describe('#hasPackage', () => {
+      beforeEach(async () => {
+        await inspector.init();
+      });
+
+      it('Returns true if package exists', () => {
+        expect(inspector.hasPackage('factory_bot_rails')).toBe(true);
+      });
+
+      it('Returns false if package does not exists', () => {
+        expect(inspector.hasPackage('fantasy_bot_rails')).toBe(false);
+      });
+    });
+
+    describe('#hasOneOfPackages', () => {
+      beforeEach(async () => {
+        await inspector.init();
+      });
+
+      it('Returns true if one of the packages exists', () => {
+        expect(inspector.hasOneOfPackages(['rspec-rails', 'rspec-off-the-rails'])).toBe(true);
+      });
+
+      it('Returns false if none of the packages exists', () => {
+        expect(inspector.hasOneOfPackages(['rspec-off-the-rails', 'rspec', '-rails'])).toBe(false);
+      });
+    });
+
+    describe('#hasPackageManagement', () => {
+      it('returns true if Gemfile is valid and present', async () => {
+        await inspector.init();
+        expect(inspector.hasPackageManagement()).toBe(true);
+      });
+
+      it('returns false if Gemfile is invalid', async () => {
+        const structure: DirectoryJSON = {
+          '/Gemfile': '...',
+        };
+
+        containerCtx.virtualFileSystemService.setFileSystem(structure);
+
+        await inspector.init();
+        expect(inspector.hasPackageManagement()).toBe(false);
+      });
+    });
+
+    describe('#hasLockFile', () => {
+      it('return false as there is no lock file', async () => {
+        await inspector.init();
+        expect(inspector.hasLockfile()).toEqual(false);
+      });
+    });
+  });
+});
---
+++ src/inspectors/package/RubyPackageInspector.ts
@@ -0,0 +1,93 @@
+import { PackageInspectorBase } from './PackageInspectorBase';
+import { IFileInspector, DependencyType, PackageVersion } from '..';
+import { inject } from 'inversify';
+import { Types } from '../../types';
+
+export class RubyPackageInspector extends PackageInspectorBase {
+  private fileInspector: IFileInspector;
+  private parsedDependencies: ParsedDependency[] = [];
+
+  constructor(@inject(Types.IFileInspector) fileInspector: IFileInspector) {
+    super();
+    this.fileInspector = fileInspector;
+  }
+
+  async init(): Promise<void> {
+    try {
+      this.debug('RubyPackageInspector init started');
+
+      // Remove any white lines in the string, but keep line breaks
+      const gemfileString = (await this.fileInspector.readFile('Gemfile')).replace(/^(?=\n)$|^\s*|\s*$|\n\n+/gm, '');
+      const gemfileLines = gemfileString.split('\n');
+      const gemfilePackages = gemfileLines
+        .map( line => line.split(' ') )
+        .map( splitLine => splitLine.map( part => part.replace(/"/g, '').replace(/,/g, '')))
+        .filter( splitLine => splitLine[0] == 'gem' && splitLine[1])
+        .map( splitLine => { return {
+          packageName: splitLine[1],
+          version: this.parseVersion(splitLine.slice(2))
+        }
+      });
+      for (const parsedPackage of gemfilePackages) {
+        if(parsedPackage.version) {
+          this.parsedDependencies.push({ packageName: parsedPackage.packageName, version: parsedPackage.version });
+        }
+      }
+      this.packages = [];
+      this.addPackages(this.parsedDependencies, DependencyType.Runtime);
+      this.debug(this.packages);
+      this.debug('RubyPackageInspector init ended');
+    } catch (e) {
+      this.packages = undefined;
+      this.debug(e);
+    }
+  }
+
+  private parseVersion(lineRemainder: string[]): PackageVersion | undefined {
+    const noVersion = PackageInspectorBase.semverToPackageVersion('0.0.0');
+    if(lineRemainder === []) {
+      // no version information provided
+      return noVersion;
+    }
+    for( const lineSegment of lineRemainder) {
+      const segmentSemVer = PackageInspectorBase.semverToPackageVersion(lineSegment);
+      if(segmentSemVer) {
+        return segmentSemVer;
+      }
+      if(lineSegment.slice(0,1) === "#") {
+        // begin comment & no version information
+        return noVersion;
+      }
+    };
+    // no version information found
+    return noVersion;
+  }
+
+  private addPackages(dependencies: ParsedDependency[] | undefined, depType: DependencyType) {
+    if (!dependencies) {
+      return;
+    }
+    if (!this.packages) {
+      this.packages = [];
+    }
+    for (const dependency of dependencies) {
+      if (dependency.version) {
+        this.packages.push({
+          dependencyType: depType,
+          name: dependency.packageName,
+          requestedVersion: dependency.version,
+          lockfileVersion: dependency.version,
+        });
+      }
+    }
+  }
+
+  hasLockfile(): boolean | undefined {
+    return false;
+  }
+}
+
+interface ParsedDependency {
+  packageName: string;
+  version: PackageVersion;
+}
