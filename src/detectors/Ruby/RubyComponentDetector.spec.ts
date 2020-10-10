1959909c14ad9f64a5fdd6629c763887b9b5afb5
PuZZleDucK <puzzleduck@gmail.com>
2020-10-10 23:18:42 +1100

    component detector for ruby

--- src/detectors/Ruby/RubyComponentDetector.spec.ts
+++ src/detectors/Ruby/RubyComponentDetector.spec.ts
@@ -1,7 +1,6 @@
 import { RubyComponentDetector } from './RubyComponentDetector';
 import { ProgrammingLanguage, ProjectComponentPlatform } from '../../model';
 import { RubyPackageInspector } from '../../inspectors/package/RubyPackageInspector';
-import { mockPackage } from '../../test/helpers/mockPackage';
 jest.mock('../../inspectors/package/RubyPackageInspector');
 
 describe('RubyComponentDetector', () => {
@@ -13,80 +12,15 @@ describe('RubyComponentDetector', () => {
     mockRubyPackageInspector = new MockedRubyPackageInspector();
   });
 
-  describe('Rails', () => {
-    it('Detects Ruby on Rails framework when particular packages are present', async () => {
-      mockRubyPackageInspector.packages = [mockPackage('rails')];
-
-      mockRubyPackageInspector.hasOneOfPackages = (packages: string[]) => {
-        const files = packages.filter((file) => file === 'rails');
-        if (files.length > 0) {
-          return true;
-        }
-        return false;
-      };
-
+  describe('Backend', () => {
+    it('Detects Ruby BackEnd', async () => {
       detector = new RubyComponentDetector(mockRubyPackageInspector);
 
       const components = await detector.detectComponent({ language: ProgrammingLanguage.Ruby, path: './src' });
 
-      expect(components.length).toEqual(1);
       expect(components[0].language).toEqual(ProgrammingLanguage.Ruby);
-      expect(components[0].path).toEqual('./');
-      expect(components[0].platform).toEqual(ProjectComponentPlatform.Rails);
+      expect(components[0].path).toEqual('./src');
+      expect(components[0].platform).toEqual(ProjectComponentPlatform.BackEnd);
     });
-  });
-
-  describe('Frontend', () => {
-    it('Detects Webpacker in Rails when particular packages are present', async () => {
-      mockRubyPackageInspector.packages = [mockPackage('webpacker')];
-
-      mockRubyPackageInspector.hasOneOfPackages = (packages: string[]) => {
-        const files = packages.filter((file) => file === 'webpacker');
-        if (files.length > 0) {
-          return true;
-        }
-        return false;
-      };
-
-      detector = new RubyComponentDetector(mockRubyPackageInspector);
-
-      const components = await detector.detectComponent({ language: ProgrammingLanguage.Ruby, path: './' });
-
-      expect(components.length).toEqual(1);
-      expect(components[0].language).toEqual(ProgrammingLanguage.Ruby);
-      expect(components[0].path).toEqual('./');
-      expect(components[0].platform).toEqual(ProjectComponentPlatform.FrontEnd);
-    });
-  });
-
-  describe('PostgreSQL', () => {
-    it('Detects PostgreSQL in Rails when particular packages are present', async () => {
-      mockRubyPackageInspector.packages = [mockPackage('pg')];
-
-      mockRubyPackageInspector.hasOneOfPackages = (packages: string[]) => {
-        const files = packages.filter((file) => file === 'pg');
-        if (files.length > 0) {
-          return true;
-        }
-        return false;
-      };
-
-      detector = new RubyComponentDetector(mockRubyPackageInspector);
-
-      const components = await detector.detectComponent({ language: ProgrammingLanguage.Ruby, path: './' });
-
-      expect(components.length).toEqual(1);
-      expect(components[0].language).toEqual(ProgrammingLanguage.Ruby);
-      expect(components[0].path).toEqual('./');
-      expect(components[0].platform).toEqual(ProjectComponentPlatform.PostgreSQL);
-    });
-  });
-
-  it('Detects NO frameworks or gems when no packages are present', async () => {
-    mockRubyPackageInspector.hasOneOfPackages = () => false;
-    detector = new RubyComponentDetector(mockRubyPackageInspector);
-
-    const components = await detector.detectComponent({ language: ProgrammingLanguage.Ruby, path: './' });
-    expect(components[0].platform).toEqual(ProjectComponentPlatform.UNKNOWN);
   });
 });
