c1b1f0c1e7ae57a28f17533813d4394bbe0eef53
Michal Vlasák <daeatel@gmail.com>
2020-12-10 22:14:00 +0100

    Fix

---
+++ action/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+if [[ $1 != "${1%bash}" || $1 != "${1%sh}" ]]; then
+  exec "$@"
+fi
+
+echo "Run DX Scanner"
+
+if [[ $1 != "${1#dx-scanner}" || $1 != "${1#dxs}" || $1 != "${1#dxscanner}" ]]; then
+  echo $@
+  exec "$@"
+else
+  echo dx-scanner run --ci -r $@
+  exec dx-scanner run --ci -r "$@"
+fi
